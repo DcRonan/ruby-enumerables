719523a7f44d17857709ac38f6243c01920fef6a
Daniel Ronan <danielconnorronan@gmail.com>
2020-08-27 10:21:37 +0100

    edit comments, added new solution

--- main.rb
+++ main.rb
@@ -3,25 +3,22 @@ module Enumerable
   def my_each(array)
     #check if block is given
     yield if block_given?
-    
-    new_array= []
+    new_array = []
 
-    #if block is given, iterate through the array/hash (don't use #each method)
+    #if block is given, iterate through the array
     for i in 0..array.length - 1
-      #push yield array/hash element to new_array_hash
+      #push yield array element to new_array
       new_array.push(yield(array[i]))
 
-      #ppush yield hash key and value to new_array_hash
-      new_array.push(yield(array[i], value))
     end
   end
+  #my_each block - for arrays
+ my_each(array) { |element| element }
 
-  #my_each block - for arrays and hashes
-  my_each(array) { |element| element }
+# times test
+  var_size.times do |i|
+    puts "hi: #{i}" 
+  end
+  print var
 
-  #my_each block - for hashes
-  #my_each(array) do |key, value|  
-    #key
-    #value
-  #end
 end
