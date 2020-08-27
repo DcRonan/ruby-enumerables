835915c480f71ef8af6e30981ab6c1546e8fc826
Losalini <C.Rokocakau@gmail.com>
2020-08-27 21:43:28 +1200

    Add incomplete my_each method

--- main.rb
+++ main.rb
@@ -1,24 +1,25 @@
 module Enumerable
   #my_each method definition
+  
   def my_each(array)
     #check if block is given
     yield if block_given?
-    new_array = []
 
     #if block is given, iterate through the array
-    for i in 0..array.length - 1
+    for i in 0..array.size() - 1
       #push yield array element to new_array
-      new_array.push(yield(array[i]))
-
+      yield(array[i])
     end
+    array
   end
-  #my_each block - for arrays
- my_each(array) { |element| element }
+
+ #my_each block - for arrays
+ my_each(array) { |element| p element * 2 }
 
 # times test
-  var_size.times do |i|
-    puts "hi: #{i}" 
-  end
-  print var
+  #var_size.times do |i|
+    #puts "hi: #{i}" 
+  #end
+  #print var
 
 end
