b9b140532af26aac715f9d1815d4c31bc1b5c9ae
Daniel Ronan <danielconnorronan@gmail.com>
2020-08-27 11:05:17 +0100

    added comments

--- main.rb
+++ main.rb
@@ -7,10 +7,13 @@ module Enumerable
 # =========  
 
 def my_each(arr)
+  # variable for the array's length
   arr_size = arr.length
+  # times loop and returns each number in the array
   arr_size.times do |i|
     yield(arr[i])
   end
+  # return initial array
   arr
 end
 
