465ce911f1e3bfe439895ddf7bf4a792db6e1767
Daniel Ronan <danielconnorronan@gmail.com>
2020-08-27 11:32:16 +0100

    linted code

--- main.rb
+++ main.rb
@@ -1,27 +1,24 @@
-# rubocop:disable Style/CaseEquality
-
 module Enumerable
+  # =========
+  #  my_each
+  # =========
 
-# =========
-#  my_each
-# =========  
-
-def my_each(arr)
-  # variable for the array's length
-  arr_size = arr.length
-  # times loop and returns each number in the array
-  arr_size.times do |i|
-    yield(arr[i])
+  def my_each(arr)
+    # variable for the array's length
+    arr_size = arr.length
+    # times loop and returns each number in the array
+    arr_size.times do |i|
+      yield(arr[i])
+    end
+    # return initial array
+    arr
   end
-  # return initial array
-  arr
-end
 
-# p my_each([1, 4, 5, 6, 100, 500, 14]) { |element| puts element }
+  # p my_each([1, 4, 5, 6, 100, 500, 14]) { |element| puts element }
 
   # ====================
   #  my_each_with_index
-  # ====================  
+  # ====================
 
   def my_each_with_index(arr)
     arr_size = arr.length
@@ -33,5 +30,5 @@ end
     arr
   end
 
-  #p my_each_with_index([1,2,3,4,5]) { |element, index| puts "#{element} and #{index}" }
+  # p my_each_with_index([1,2,3,4,5]) { |element, index| puts "#{element} and #{index}" }
 end
