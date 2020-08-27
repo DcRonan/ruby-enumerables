4129dafc82517061b56d804cc7925c0ae7bb2b18
Losalini <C.Rokocakau@gmail.com>
2020-08-27 22:25:54 +1200

    Add my_each_with_index solution

--- main.rb
+++ main.rb
@@ -17,6 +17,21 @@ def my_each(arr)
   arr
 end
 
-# p test([1, 4, 5, 6, 100, 500, 14]) { |element| puts element }
+# p my_each([1, 4, 5, 6, 100, 500, 14]) { |element| puts element }
 
+  # ====================
+  #  my_each_with_index
+  # ====================  
+
+  def my_each_with_index(arr)
+    arr_size = arr.length
+    # times loop and returns each number in the array
+    arr_size.times do |i|
+      yield(arr[i], i)
+    end
+    # return initial array
+    arr
+  end
+
+  #p my_each_with_index([1,2,3,4,5]) { |element, index| puts "#{element} and #{index}" }
 end
